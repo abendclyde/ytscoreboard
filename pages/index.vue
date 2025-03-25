6614bfc26789abd1f21c5cce7d90ecc0e7365235
clyde <mail@abendcly.de>
2025-03-25 18:30:04 +0100

    updated points

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="relative w-3/7 flex gap-4" v-for="point in points">
-      <UCard class="w-full" variant="subtle">
-        <UCheckbox size="xl" :label="point"/>
+  <div>
+    <UContainer class="relative w-3/7 flex flex-col gap-2">
+      <UCard class="w-full" variant="subtle" v-for="point in points">
+        <UCheckbox size="xl" :label="point" />
       </UCard>
+    </UContainer>
   </div>
 </template>
 
 <script lang="ts" setup>
-  const points = ["Hurensohn TV","E-Pumpe","50 KW Heizleistung", "Penispumpe"]
+const points = [  	"Hurensohn TV oder Bastard Beamer", "E-Pumpe", "50 KW Heizleistung", "Penispumpe", "Headset", "Sinnloses Gadget", "Propellör", "Place to Keul", 
+					"Einparkhilfe", "Halbe Küche", "Abendkaffee", "Wecker", "Sonne", "Regen", "Schnee", "Funny Moment", "Gericht mit 30 Zutaten", "Fast verbrannt", "Alki Moment", 
+                  	"Teleskop-Lampe", "Unstable Construction", "Raumspritzer", "80° Essen", "Cute Moment", "Alle Fenster auf", "Fettsack"]
 </script>
 
 <style></style>
