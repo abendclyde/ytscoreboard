a543c2323bce0b3085a22da3d857dd310a89bb07
Sophtli <mail@sopht.li>
2025-04-11 01:10:14 +0200

    improve eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,10 @@
 import vue from 'eslint-plugin-vue'
-// @ts-expect-error - no types
-import drizzle from 'eslint-plugin-drizzle'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
 	rules: {
 		'vue/multi-word-component-names': 'off',
-		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/no-unused-vars': 'off',
 	},
 })
 	.prepend(vue.configs['flat/recommended'])
-	.prepend(drizzle.configs['flat/recommended'])
